f2d86933ced29a1cbccebe470239e9840e8e1b72
n0F4x <ggabor2002@gmail.com>
2024-05-27 18:06:23 +0200

    Compile a simple mesh shader

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -13,7 +13,7 @@ try {
         .add_plugin<plugins::Logger>(plugins::Logger::Level::eTrace)
         .add_plugin<plugins::Cache>()
         .add_plugin<plugins::Window>(
-            1'280, 720, "My window", plugins::Window::default_configure
+            1'280, 720, "Mesh shading", plugins::Window::default_configure
         )
         .add_plugin<plugins::Renderer>(plugins::Renderer::Options{}.request_dependencies(
             MeshRenderer::create_dependency_provider()
